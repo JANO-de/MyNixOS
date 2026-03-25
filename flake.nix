ea8f521f2933d2369707bcb0dfa6f1266a50f429
JANO-de <jan01sp@proton.me>
2026-03-25 03:13:48 +0100

    e

--- flake.nix
+++ flake.nix
@@ -19,6 +19,9 @@
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake
-    { inherit inputs; }
+    { 
+      systems = [ "x86_64-linux" ];  
+      inherit inputs; 
+    }
     (inputs.import-tree ./modules);
 }
