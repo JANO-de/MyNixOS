9f63d617cb887221ef44b445709d2b303e574878
JANO-de <jan01sp@proton.me>
2026-03-27 13:32:35 +0100

    Added gaming compatibility

--- modules/core/gaminiglaptop.nix
+++ modules/core/gaminiglaptop.nix
@@ -8,13 +8,5 @@
 
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia.modesetting.enable = true;
-
-    hardware.nvidia.prime = {
-      sync.enable = true;
-
-      intelBusId = "PCI:0:0:2";
-
-      nvidiaBusId = "PCI:0:1:0";
-    };
   };
 }
