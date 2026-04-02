341466b072a8a01cb54b1bb6883287d62f49c8b4
JANO-de <jan01sp@proton.me>
2026-04-02 22:26:09 +0200

    ADA

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -32,11 +32,6 @@
       config.common.default = "*";
     };
 
-    services.desktopManager.plasma6.enable = true;
-    environment.plasma6.excludePackages = with pkgs.kdePackages; [
-      konsole
-    ];
-
     nixpkgs.config.allowUnfree = true;
 
     security.polkit.enable = true;
