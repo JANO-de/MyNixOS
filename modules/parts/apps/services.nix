2e6a6f23ac85404510bb2774f264701509bfa1b2
JANO-de <jan01sp@proton.me>
2026-04-03 15:31:14 +0200

    modified:   modules/parts/apps/services.nix

--- modules/parts/apps/services.nix
+++ modules/parts/apps/services.nix
@@ -7,12 +7,6 @@
       gvfs.enable = true;
     };
 
-    systemd.user.services.my-ags = {
-      description = "AGS Shell (Noctalia-like)";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig.ExecStart = "${self.packages.${pkgs.system}.myAgs}/bin/my-ags-shell";
-    };
-
     services.udev.extraRules = ''
       # MediaTek BROM (The KingKong 9 ID)
       SUBSYSTEM=="usb", ATTR{idVendor}=="0e8d", ATTR{idProduct}=="0003", MODE="0666", GROUP="adbusers"
