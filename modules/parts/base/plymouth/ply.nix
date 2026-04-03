6cb19b5d0453bd58b8de56e72ff4acd6b3969eab
JANO-de <jan01sp@proton.me>
2026-04-03 14:57:20 +0200

    modified:   modules/parts/base/plymouth/ply.nix

--- modules/parts/base/plymouth/ply.nix
+++ modules/parts/base/plymouth/ply.nix
@@ -1,41 +1,38 @@
 { self, inputs, ... }: {
-  # Definimos el módulo de Plymouth
   flake.nixosModules.ply = { pkgs, config, lib, ... }: 
     let
-      # El "let" va ANTES de abrir el set de configuración final
-      hunt-showdown-plymouth = pkgs.stdenv.mkDerivation {
-        pname = "hunt-showdown-plymouth";
-        version = "1.0";
-
+      # Use runCommand instead of stdenv.mkDerivation to avoid Bash dependencies
+      hunt-showdown-plymouth = pkgs.runCommand "hunt-showdown-plymouth" {
         src = pkgs.fetchFromGitHub {
           owner = "Anxhul10";
           repo = "huntShowdown-plymouth";
           rev = "master";
           sha256 = "sha256-zBaz6LWan6ExXfWv5nRGjamfV47SpfQSxvS/jQ8G4Ck="; 
         };
-
-        dontPatchELF = true;
-        dontRewriteMode = true;
-
-        installPhase = ''
-          mkdir -p $out/share/plymouth/themes/huntShowdown-plymouth
-          cp -r * $out/share/plymouth/themes/huntShowdown-plymouth
-          
-          # Ajustamos las rutas eliminando cualquier referencia absoluta a /usr
-          find $out -type f -name "*.plymouth" -exec sed -i "s|/usr/share/plymouth/themes/huntShowdown-plymouth|$out/share/plymouth/themes/huntShowdown-plymouth|g" {} +
-          find $out -type f -name "*.script" -exec sed -i "s|/usr/share/plymouth/themes/huntShowdown-plymouth|$out/share/plymouth/themes/huntShowdown-plymouth|g" {} +
-        '';
-      };
+      } ''
+        # 1. Create the destination folder
+        # The theme name in the files is 'hunt', so the folder MUST be 'hunt'
+        mkdir -p $out/share/plymouth/themes/huntShowdown-plymouth
+        
+        # 2. Copy the contents
+        cp -r $src/* $out/share/plymouth/themes/huntShowdown-plymouth
+        chmod -R +w $out/share/plymouth/themes/huntShowdown-plymouth
+        
+        # 3. Fix the hardcoded paths inside the theme files
+        # We use 'sed' to point Plymouth to the Nix Store instead of /usr
+        sed -i "s|/usr/share/plymouth/themes/huntShowdown-plymouth|$out/share/plymouth/themes/huntShowdown-plymouth|g" $out/share/plymouth/themes/huntShowdown-plymouth/huntShowdown-plymouth.plymouth
+        sed -i "s|/usr/share/plymouth/themes/huntShowdown-plymouth|$out/share/plymouth/themes/huntShowdown-plymouth|g" $out/share/plymouth/themes/huntShowdown-plymouth/huntShowdown-plymouth.script
+      '';
     in
     {
-      # Aquí empieza la configuración real de NixOS
       boot.plymouth = {
         enable = true;
         themePackages = [ hunt-showdown-plymouth ];
-        theme = "huntShowdown-plymouth";
+        # This must match the filename 'hunt.plymouth' inside the folder
+        theme = "huntShowdown-plymouth"; 
       };
 
-      # Configuraciones para NVIDIA
+      # Essential for NVIDIA + Plymouth
       boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
       boot.kernelParams = [ "quiet" "splash" "nvidia-drm.modeset=1" ];
     };
