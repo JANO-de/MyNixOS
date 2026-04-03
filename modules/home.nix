f30e60344514da730d34a08f58353bf7cbce6192
JANO-de <jan01sp@proton.me>
2026-04-03 16:25:34 +0200

    modified:   modules/home.nix

--- modules/home.nix
+++ modules/home.nix
@@ -1,17 +1,14 @@
 { inputs, pkgs, ... }:
 {
-  # add the home manager module
   imports = [ inputs.ags.homeManagerModules.default ];
 
   programs.ags = {
     enable = true;
-
-    # symlink to ~/.config/ags
     configDir = ./parts/base/ags;
 
-    # additional packages and executables to add to gjs's runtime
     extraPackages = with pkgs; [
-      inputs.astal.packages.${pkgs.system}.battery
+      # Use the system attribute from the pkgs object passed to the module
+      inputs.astal.packages.${system}.battery 
       fzf
     ];
   };
